44f21042a908935f309d6cfce7505b44aa03b98f
yunengwu1926 <931708230@qq.com>
2022-12-06 20:21:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,116 @@
+**需要完整代码可以加qq  931708230 或者加微信 ynwwxid**
+
+#疫情物资管理系统 #java web #java #毕业设计 #课程设计 #mybatis #Springboot  #vue #mysql #源代码
+
+## 基于Springboot+vue的疫情物资管理系统(源代码+数据库)
+
+## 一、系统介绍
+本项目分为管理员与普通用户两种角色
+
+管理员角色包含以下功能：
+
+- 系统管理
+  控制面板、用户管理、菜单权限管理、部门管理、角色管理、图标管理
+- 业务管理
+  物资去处管理、入库记录管理、物资资料管理、物资类别管理、物资来源管理、发放记录管理、物资库存管理
+- 健康报备
+  全国疫情速报、健康打卡
+- 请假管理
+  操作日志、登录日志、系统接口
+
+用户角色包含以下功能：
+- 业务管理
+  物资去处管理、入库记录管理、物资资料管理、物资类别管理、物资来源管理、发放记录管理、物资库存管理
+- 健康报备
+  全国疫情速报、健康打卡
+
+## 二、所用技术
+
+后端技术栈：
+
+- springboot
+- mybatis
+- shiro
+- mysql
+- jwt
+
+前端技术栈：
+
+- vue全家桶
+- elment-ui
+- echarts
+- axios
+
+
+## 三、环境介绍
+
+基础环境 :IDEA/eclipse, JDK 1.8, Mysql5.7及以上,Node.js(v12.9.1版本),Maven
+
+所有项目以及源代码本人均调试运行无问题 可支持远程调试运行
+
+## 四、页面截图
+
+![contents](./picture/picture8.png)
+
+![contents](./picture/picture1.png)
+
+![contents](./picture/picture2.png)
+
+![contents](./picture/picture3.png)
+
+![contents](./picture/picture4.png)
+
+![contents](./picture/picture5.png)
+
+![contents](./picture/picture6.png)
+
+![contents](./picture/picture7.png)
+
+
+
+![contents](./picture/picture9.png)
+
+![contents](./picture/picture10.png)
+
+![contents](./picture/picture11.png)
+
+![contents](./picture/picture12.png)
+
+![contents](./picture/picture13.png)
+
+![contents](./picture/picture14.png)
+
+![contents](./picture/picture15.png)
+
+![contents](./picture/picture16.png)
+
+![contents](./picture/picture17.png)
+
+![contents](./picture/picture18.png)
+
+
+## 五、浏览地址
+
+前端访问地址：http://localhost:8080/#/login
+
+用户账号/密码：lisi/123456
+
+管理员账号/密码：zhangsan/123456
+
+## 六、安装教程
+
+1. 使用Navicat或者其它工具、在mysql中创建对应名称的数据库、并导入项目的sql文件；
+2. 使用IDEA/Eclipse导入项目，若为maven项目请选择maven，导入成功后请执行maven clean;maven install命令
+3. 修改back后端项目里面的application-dev.yml 里面的数据库配置，修改UploadServiceImpl里面的PATH路径
+4. 启动项目后端项目 
+5. vscode打开xinguan-vue-main项目
+6. 打开终端，执行npm install 依赖下载完成后执行 npm run dev,执行成功后会显示访问地址（注意node一定要安装12.9.1版本）
+
+**需要完整代码可以加qq  931708230 或者加微信 ynwwxid**
+
+**需要完整代码可以加qq  931708230 或者加微信  ynwwxid**
+
+
+
+
+
---
+++ back/xinguan-web/src/main/java/com/coderman/config/SwaggerConfig.java
@@ -0,0 +1,68 @@
+package com.coderman.config;
+
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author zhangyukang
+ * @Date 2020/11/11 20:22
+ * @Version 1.0
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.coderman"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .build())
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+
+    private List<ApiKey> securitySchemes() {
+        ArrayList<ApiKey> apiKeyList = Lists.newArrayList();
+        apiKeyList.add(new ApiKey("JSON WEB TOKEN(秘钥)", "Authorization", "header"));
+        return apiKeyList;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts=new ArrayList<>();
+        securityContexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build());
+        return securityContexts;
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences=new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
+    }
+}
